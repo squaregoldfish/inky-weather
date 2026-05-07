f370ab2a8adbaa3740063b6544bd59300e8dcf79
Steve Jones <steve@squaregoldfish.co.uk>
2026-05-07 19:45:46 +0200

    Image creation loop

--- create.sh
+++ create.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
 # Get data and create images for display
-
 source .venv/bin/activate
 
-# Retrieve Netatmo
-python get_netatmo.py
+MIN_TIME=600
 
-# Get Open Meteo forecast
-python get_open_meteo.py
+last_hour=-1
 
-# Get pressure data and preprocess
-python get_pressure.py
-wgrib2 pressure.grib2 -netcdf pressure.nc
+while true
+do
 
+    # Record the start time of the iteration
+    start=`date +%s`
 
-# Draw main weather map
-python weather_map.py
+    # Some things we only get once per hour
+    current_hour=$(date +%H)
 
-# Rain map (for dashboard display)
-python rain_map.py
+    if [[ "$current_hour" != "$last_hour" ]]; then
+        # Get Open Meteo forecast
+        python get_open_meteo.py
 
-# Draw dashboards
-python dashboard.py
+        # Get pressure data and preprocess
+        python get_pressure.py
+        wgrib2 pressure.grib2 -netcdf pressure.nc
+    fi
+
+    last_hour=$current_hour
+
+    # Retrieve Netatmo
+    python get_netatmo.py
+
+    # Draw main weather map
+    python weather_map.py
+
+    # Rain map (for dashboard display)
+    python rain_map.py
+
+    # Draw dashboards
+    python dashboard.py
+
+    end=`date +%s`
+
+    elapsed=$((end - start))
+    if [ "$elapsed" -ge "$MIN_TIME" ]
+    then
+        continue
+    else
+        wait_time=$((MIN_TIME - elapsed))
+        sleep "$wait_time"
+    fi
+done
